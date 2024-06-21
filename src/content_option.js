3aa0b6f71753328bf0678f8cde06497dc28d95db
farnaz <109223719+farnaz-vand@users.noreply.github.com>
2024-06-21 11:12:12 +0330

    Add files via upload

---
+++ src/content_option.js
@@ -0,0 +1,225 @@
+const logotext = "FARNAZ";
+const meta = {
+    title: "FARNAZ BANAVAND",
+    description: "I’m Farnaz Banavand front end developer ,currently working in Iran",
+};
+
+const introdata = {
+    title: "I’m Farnaz Banavand",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop front end ",
+    },
+    description: "Passionate front-end developer dedicated to crafting captivating user experiences through cutting-edge technologies and meticulous attention to detail.",
+    your_img_url: "",
+};
+
+const dataabout = {
+    title: "A bit about myself",
+    aboutme: `As a professional front-end developer, I bring 2 years of experience with a focus on REACT JS, Next js,
+             and TAILWIND CSS. With a proven ability to craft responsive and visually appealing user interfaces, I am
+             a collaborative team player well-versed in the full development lifecycle.`,
+};
+
+const worktimeline = [{
+        jobtitle: "Freelancer Front End Developer",
+        where: "Remote",
+        date: "APRIL 2022-PERESENT",
+    },
+    {
+        jobtitle: "Intern Front End Developer",
+        where: "Tehran - PAYDAR SAMANEH",
+        date: "OCTOBER 2023-MARCH 2024",
+    },
+    // {
+    //     jobtitle: "Designer of week",
+    //     where: "ALquds",
+    //     date: "2019",
+    // },
+];
+
+const skills = [{
+        name: "TypeScript",
+        
+    },
+    {
+        name: "react js",
+        
+    },
+    {
+        name: "Next js",
+        
+    },
+    {
+        name: "Qwik js",
+       
+    },
+    {
+        name: "Context Api",
+        
+    },
+
+{
+    name: "Redux Toolkit",
+    
+},
+{
+    name: "Zustand",
+    
+},
+{
+    name: "FormiK",
+   
+},
+{
+    name: "React Hook Form",
+    
+},
+{
+    name: "Tailwind",
+    
+},
+{
+    name: "Material UI",
+    
+},
+{
+    name: "Fetch Api",
+   
+},
+{
+    name: "Axios",
+    
+},
+{
+    name: "Json",
+    
+},
+{
+    name: "GitHub",
+    
+},
+{
+    name: "Linux",
+   
+},
+{
+    name: "Docker",
+    
+},
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Mobile Apps",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Wordpress Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+];
+
+const dataportfolio = [
+    
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "Welcome to our hotel website, where comfort meets elegance",
+        link: "https://yourhotel.vercel.app/",
+        link2: "https://github.com/farnaz-vand/Hotelme-/tree/main"
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "CORPORATE WEBSITE.",
+        link: "https://avayevokala.com/",
+        link2: "https://github.com/PaydarNode"
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "CORPORATE WEBSITE.",
+        link: "https://osouli.trade/",
+        link2: "https://github.com/HolismInternship/Farnazvand"
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "CORPORATE WEBSITE.",
+        link: "https://osouli.trade/",
+        link2: "https://github.com/HolismInternship/Farnazvand"
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "METAVERSE WEBSITE.",
+        link: "https://metaverse-madness-site.vercel.app/",
+        link2: "https://github.com/farnaz-vand/mymetaversapp"
+    },
+    // {
+    //     img: "https://picsum.photos/400/700/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+
+    // {
+    //     img: "https://picsum.photos/400/600/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/300/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/550/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/700/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "banavandfarnaz@gmail.com",
+    YOUR_FONE: "(+98)916-432-3137",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/farnaz-vand",
+    facebook: "https://facebook.com",
+    linkedin: "http://www.linkedin.com/in/farnaz-banavand-b68320211",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
